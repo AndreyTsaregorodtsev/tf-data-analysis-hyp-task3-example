0487200afe472880ce2d7f59983d234adf46241a
AndreyTsaregorodtsev <71283873+AndreyTsaregorodtsev@users.noreply.github.com>
2023-04-11 00:05:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-from statsmodels.stats.weightstats import ztest
+from scipy.stats import ttest_ind
 
 chat_id = 297386717 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    p = ztest(x,y)[1]
+    p = ttest_ind(x,y).pvalue
     if(p<0.09):
-      return False
-    else:
       return True
+    else:
+      return False
